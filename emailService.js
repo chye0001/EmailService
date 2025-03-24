d4fd09f92a11a0e638117a7bc7e3c0b5c88a7074
Chris Ye <chye0001@kea.stud.dk>
2025-03-24 12:27:50 +0100

    implimented nodemailer in emailService.js

--- emailService.js
+++ emailService.js
@@ -1,1 +1,24 @@
-let transporter = nodemailer.createTransport(transport[, defaults])
+import nodemailer from 'nodemailer';
+
+const testAccount = await nodemailer.createTestAccount();
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false,
+  auth: {
+    user: testAccount.user,
+    pass: testAccount.pass,
+  },
+});
+
+const info = await transporter.sendMail({
+  from: '"Your Name" <your.email@example.com>',
+  to: 'recipient@example.com',
+  subject: 'Hello ✔',
+  text: 'Hello world?',
+  html: '<b>Hello world?</b>',
+});
+
+console.log('Message id: %s', info.messageId);
+console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
